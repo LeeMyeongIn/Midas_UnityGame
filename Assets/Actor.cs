769fa1cab6057f245274732e8b40da34e78c1ae1
HCY9 <gksc098@gmail.com>
2025-04-03 15:41:15 +0900

    15 Dialogue system

---
+++ Assets/Actor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Dialogue/Acotr")]
+public class Actor : ScriptableObject
+{
+    public string Name;
+    public Sprite portrait;
+}
