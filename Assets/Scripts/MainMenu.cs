2641c53772c6f544ecae78cfe8a6f8474cbf2b06
yunaim12 <164979353+yunaim12@users.noreply.github.com>
2025-05-04 20:30:47 +0900

    50~ 53 Weather part 1 Rain

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] string nameEssentialScene;
+    [SerializeField] string nameNewGameStartScene;
+   public void ExitGame()
+    {
+        Debug.Log("Quitting the game!");
+        Application.Quit(); 
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(nameNewGameStartScene, LoadSceneMode.Single);
+        SceneManager.LoadScene(nameEssentialScene, LoadSceneMode.Additive);
+       
+    }
+}
