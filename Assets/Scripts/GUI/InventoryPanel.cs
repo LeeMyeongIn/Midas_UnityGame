ba1d1099d3b37a19d4163285634658635a64e9cd
LeeMyeongIn <dulsfnj413@gmail.com>
2025-06-09 03:55:38 +0900

    인벤토리 아이템 Drag&Drop

--- Assets/Scripts/GUI/InventoryPanel.cs
+++ Assets/Scripts/GUI/InventoryPanel.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InventoryPanel : ItemPanel
+public class InventoryPanel : ItemPanel, IDropHandler
 {
     public override void OnClick(int id)
     {
@@ -12,5 +13,27 @@ public class InventoryPanel : ItemPanel
         Show();
     }
 
-}
+    public void OnDrop(PointerEventData eventData)
+    {
+        // Drag 중인 아이템 정보 가져오기
+        ItemSlot dragSlot = GameManager.instance.dragAndDropController.itemSlot;
 
+        if (dragSlot == null || dragSlot.item == null)
+        {
+            Debug.Log("[InventoryPanel.OnDrop] 드래그 중인 아이템이 없음");
+            return;
+        }
+
+        Debug.Log($"[InventoryPanel.OnDrop] {dragSlot.item.Name} 아이템 {dragSlot.count}개 드랍 시도");
+
+        // 인벤토리에 아이템 추가
+        inventory.Add(dragSlot.item, dragSlot.count);
+
+        // Drag 상태 초기화
+        dragSlot.Clear();
+        GameManager.instance.dragAndDropController.UpdateIcon();
+
+        // 인벤토리 다시 갱신
+        Show();
+    }
+}
